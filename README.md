78e03bba3bece39820b030a4e4013b7cd1f628c0
Josh Black <joshuatblack@ufl.edu>
2014-08-25 13:29:33 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Usage:
+Can use this by doing `node app.js --harmony` or `node app.js --harmony URL_FOR_CURRENT_REGISTRAR`
+
+Library:
+`departments`: Find a listing of all the departments in the registrar
+`env`: Builds a new environment that we can perform jQuery actions on
+`courses`: Parse a page and get the appropriate information
---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var Promise = require('bluebird');
+var baseURL = process.argv[3] || 'http://registrar.ufl.edu/soc/201408/all/';
+var builder = require('./lib/departments')(baseURL);
+var env = require('./lib/env');
+var courses = require('./lib/courses');
+
+/* TODO */
+// Commit each new department that we parse to a database
+
+// Get all the departments currently in the registrar
+builder.then(function(depts) {
+    // iterate through our list of departments
+    // depts.forEach(function(e) {
+    //     var page = env(e, 'http://code.jquery.com/jquery.js');
+    //     // once we grab the page parse it's contents
+    //     // get course information
+    //     // save course information
+    // });
+
+    // Get the page corresponding to the department URL
+    // var page = env(depts[0], 'http://code.jquery.com/jquery.js');
+
+    // page.then(function($) {
+    //     // Get the data for the corresponding page instance
+    //     courses($).then(function(data) {
+
+    //         // Commit the course data to our database
+    //         db.store(data);
+    //     });
+    // });
+});
+
+// builder.then(function(depts) {
+//     depts.forEach(function(e) {
+//         var page = env(e, 'http://code.jquery.com/jquery.js');
+
+//         page.then(function($) {
+//             return courses($);
+//         }).then(function(data) {
+//             return db.store(data);   
+//         });
+//     });    
+// });
